c79a0a06a1c85d4cffaebfe5c6fa9968084508e7
Allen Hai <allen@zeit.co>
2020-08-12 19:39:18 -0500

    remove vercel.json

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', function(req, res) {
+app.get('/api', function(req, res) {
     res.json({ message: "hello world" })
 })
 
-app.get('/products', function(req, res) {
+app.get('/api/products', function(req, res) {
     res.json([{ name: 'Macbook', price: 1500 }, { name: 'Backpack', price: 80 }])
 })
 
